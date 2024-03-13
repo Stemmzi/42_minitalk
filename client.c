fb2caee3172447f0d8e227143d1d3fc305189fc6
Soren Geiger <soerengeiger@gmx.de>
2024-03-13 18:55:39 +0100

    first test version

--- client.c
+++ client.c
@@ -6,7 +6,47 @@
 /*   By: sgeiger <sgeiger@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 22:16:00 by sgeiger           #+#    #+#             */
-/*   Updated: 2024/03/12 22:16:01 by sgeiger          ###   ########.fr       */
+/*   Updated: 2024/03/13 18:11:53 by sgeiger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include "libft/libft.h"
+
+void	send_bits(int pid, char *str, size_t len)
+{
+	int		shift;
+	size_t	i;
+
+	i = 0;
+	while (i <= len - 1)
+	{
+		shift = 0;
+		while (shift < 7)
+		{
+			if ((str[i] >> shift) & 1)
+				kill(pid, SIGUSR2);
+			else
+				kill(pid, SIGUSR1);
+			shift++;
+			usleep(300);
+		}
+		i++;
+	}
+}
+
+int	main(int argc, char *argv[])
+{
+	int		pid;
+	char	*str;
+
+	if (argc == 3)
+	{
+		pid = ft_atoi(argv[1]);
+		str = argv[2];
+		send_bits(pid, str, ft_strlen(str));
+	}
+	else
+		return (1);
+	return (0);
+}
--- server.c
+++ server.c
@@ -6,7 +6,37 @@
 /*   By: sgeiger <sgeiger@student.42heilbronn.de    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/12 22:15:48 by sgeiger           #+#    #+#             */
-/*   Updated: 2024/03/12 22:15:49 by sgeiger          ###   ########.fr       */
+/*   Updated: 2024/03/13 18:48:04 by sgeiger          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <signal.h>
+#include "libft/libft.h"
+#include <unistd.h>
+
+void	handler(int num)
+{
+	if (num == SIGUSR1)
+	{
+		write(1, "0", 1);
+	}
+	if (num == SIGUSR2)
+	{
+		write(1, "1", 1);
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	sa;
+
+	sa.sa_handler = handler;
+	ft_printf("%d\n", getpid());
+	while (1)
+	{
+		sigaction(SIGUSR1, &sa, NULL);
+		sigaction(SIGUSR2, &sa, NULL);
+		pause();
+	}
+	return (0);
+}
